b39f59d7c177278f21e03c4f789c8238c72e5231
tahaftm <tahatariqf@gmail.com>
2026-07-23 23:42:06 -0700

    ready to deploy

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ import numpy as np
 import dill
 import sys
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -19,11 +20,15 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
 
-def evaluate_model(X_train,y_train,X_test, y_test, models): 
+def evaluate_model(X_train,y_train,X_test, y_test, models, params): 
     try:
         report = {}
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            param = params[list(models.keys())[i]]
+            gscv = GridSearchCV(model,param, cv = 3)
+            gscv.fit(X_train, y_train) 
+            model.set_params(**gscv.best_params_)
             model.fit(X_train, y_train) # Train model
 
             # Make predictions
@@ -36,5 +41,12 @@ def evaluate_model(X_train,y_train,X_test, y_test, models):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+    except Exception as e:
+        raise CustomException(e,sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
     except Exception as e:
         raise CustomException(e,sys)
